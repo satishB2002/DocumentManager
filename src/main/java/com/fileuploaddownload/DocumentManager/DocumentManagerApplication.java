003ba1ebc2f2865abce12ddd7e45d0485c4483d6
satishB2002 <127237435+satishB2002@users.noreply.github.com>
2023-06-20 22:56:28 +0530

    download all types file

---
+++ src/main/java/com/fileuploaddownload/DocumentManager/DocumentManagerApplication.java
@@ -0,0 +1,15 @@
+package com.fileuploaddownload.DocumentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentManagerApplication.class, args);
+		System.out.println("Hii");
+	}
+
+}
+
